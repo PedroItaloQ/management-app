3ca238be5afe5b0d142d9669dbe925661e61d7ba
PedroItaloQ <peedroitalo2906@gmail.com>
2024-07-25 11:53:38 -0300

    feat: Created _layput.tsx

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Slot } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
+import { initializeDatabase } from "../database/initializeDatabase";
+
+export default function Layout() {
+  return (
+    <SQLiteProvider databaseName="myDatabase.db" onInit={initializeDatabase}>
+      <Slot />
+    </SQLiteProvider>
+  );
+}
